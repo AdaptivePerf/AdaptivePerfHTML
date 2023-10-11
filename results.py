ad726c27a64ececb93a1535ecfcf8b1968c2107b
Maksymilian Graczyk <maksymilian.graczyk@cern.ch>
2023-10-11 19:33:58 +0200

    Replace manual parsing with perf-based parsing and add runtime length processing

--- results.py
+++ results.py
@@ -1,4 +1,6 @@
 import re
+import pickle
+import subprocess
 from treelib import Tree
 from pathlib import Path
 
@@ -55,101 +57,30 @@ class ProfilingResults:
                         sorted(id_str_list, key=lambda x: x[1])))
 
     def __init__(self, identifier: str):
-        path = Path(PROFILING_STORAGE) / identifier
-
-        self._syscalls_data = (path / 'syscalls.data').read_text()
+        self._path = Path(PROFILING_STORAGE) / identifier
         self._thread_tree = None
 
     def get_thread_tree(self) -> Tree:
         if self._thread_tree is not None:
             return self._thread_tree
 
-        start_time = None
-        tree = Tree()
+        r = subprocess.run(['/data/mgraczyk/linux/tools/perf/perf', 'script', '-i',
+                            'syscalls.data', '-s',
+                            Path(__file__).resolve().parent / 'perf-script.py'],
+                           cwd=self._path, stdout=subprocess.PIPE)
 
-        tmp_dict = {}
-        pid_dict = {}
+        r.check_returncode()
 
-        def get_return_val(line, body):
-            return_match = re.search(r'\s*\=\s*([0-9]+)(?:\s*\(\S*\))?$', body)
+        tree = pickle.loads(r.stdout)
+        l = list(map(lambda x: x.tag[-1],
+                     tree.all_nodes_itr()))
+        minimum, maximum = min(l), max(l)
 
-            if return_match is None:
-                raise RuntimeError((line, body))
-
-            return int(return_match.group(1))
-
-        for line in self._syscalls_data.split('\n'):
-            line = line.strip()
-            if len(line) == 0:
-                continue
-
-            match = re.search(r'^\s*([0-9\.\?]+).+?\:\s*(.+?)/([0-9]+)\s*([a-z0-9]+|\.\.\.)(.+)', line)
-
-            if match is None:
-                raise RuntimeError(line)
-
-            t = None if match.group(1) == '?' \
-                else int(match.group(1).replace('.', ''))
-            name = match.group(2)
-            pid = int(match.group(3))
-            syscall = match.group(4)
-            body = match.group(5)
-
-            ALLOWED = ['perf', 'python3.12']
-
-            if name not in ALLOWED and \
-               name not in [f'{pid}', f':{pid}']:
-                continue
-
-            if syscall == '...':
-                if get_return_val(line, body) == 0:
-                    continue
-
-                if pid not in tmp_dict:
-                    raise RuntimeError((line, pid))
-
-                syscall = tmp_dict[pid][0]
-                body = tmp_dict[pid][1] + re.sub(r'\s*\[continued\]\:\s*[a-z0-9]+\(.*?\)\)', '', body)
-
-                if t is None:
-                    t = tmp_dict[pid][2]
-
-                line += ' ***AND*** ' + tmp_dict[pid][3]
-            elif body.endswith('...'):
-                tmp_dict[pid] = (syscall, body[:-3], t, line)
-                continue
-
-            if syscall != 'clone':
-                raise RuntimeError((line, syscall))
-
-            match_flags = re.search(r'clone_flags\: (\S+)', body)
-
-            if match_flags is None:
-                raise RuntimeError((line, body))
-
-            flags = match_flags.group(1).replace(',', '').split('|')
-
-            return_val = get_return_val(line, body)
-
-            if name == 'perf' and '0x11' in flags:
-                start_time = t
-                code = f'{return_val}/{return_val}'
-                pid_dict[return_val] = code
-                tree.create_node(f'{code} (at 0 &micro;s)', code)
-                tree.root = code
-            elif pid in pid_dict:
-                calling = pid_dict[pid]
-
-                if 'THREAD' in flags:
-                    code = f'{calling.split("/")[0]}/{return_val}'
-                else:
-                    code = f'{return_val}/{return_val}'
-
-                if 'PARENT' in flags:
-                    raise NotImplementedError(line)
-
-                pid_dict[return_val] = code
-                tree.create_node(f'{code} (at {t - start_time:,} &micro;s)', code, parent=calling)
+        for n in tree.all_nodes_itr():
+            if n.tag[-1] == -1 or minimum == maximum:
+                n.tag += (0,)
+            else:
+                n.tag += (round((n.tag[-1] - minimum) * 510 / (maximum - minimum)),)
 
         self._thread_tree = tree
         return tree
