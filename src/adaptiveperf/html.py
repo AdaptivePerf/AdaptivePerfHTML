a54275caadfc3d7676a2701c6734858108f8bd9a
Maksymilian Graczyk <maksymilian.graczyk@cern.ch>
2023-12-13 11:17:54 +0100

    Fix bugs and improve formatting in html.py

--- src/adaptiveperf/html.py
+++ src/adaptiveperf/html.py
@@ -14,24 +14,27 @@ def create_html(results_path):
     ids = ProfilingResults.get_all_ids(results_path)
 
     index_html = template.render(ids=ids, local=True,
-                                 get_tree=lambda x: html.escape(ProfilingResults(results_path,
-                                                                                 x).get_json_tree(), quote=True))
+                                 get_tree=lambda x: html.escape(
+                                     ProfilingResults(results_path,
+                                                      x).get_json_tree(),
+                                     quote=True))
 
     with (results_path / 'index.html').open(mode='w') as f:
         f.write(index_html)
 
-    script_files = (Path(__file__).resolve().parent / 'static').glob('vis-timeline-graph2d.min*')
+    script_files = (Path(__file__).resolve().parent / 'static').glob(
+        'vis-timeline-graph2d.min*')
 
     for script_file in script_files:
-        shutil.copy(script_file, path)
+        shutil.copy(script_file, results_path)
 
 
 def main():
     if len(sys.argv) != 2 or sys.argv[1] in ['-h', '--help']:
         print('Exactly one argument is required: the path to the "results" '
               'directory created by AdaptivePerf.', file=sys.stderr)
-        print('You can also run adaptiveperf-html with "-h"/"--help" to print this '
-              'message.', file=sys.stderr)
+        print('You can also run adaptiveperf-html with "-h"/"--help" to print '
+              'this message.', file=sys.stderr)
         sys.exit(1)
 
     path_str = sys.argv[1]
@@ -41,9 +44,9 @@ def main():
         print(f'{path} does not exist!', file=sys.stderr)
         sys.exit(1)
 
-    create_html(path_str)
+    create_html(path)
 
-    print(f'Done! You can open {path.resolve() / "results" / "index.html"} '
+    print(f'Done! You can open {path.resolve() / "index.html"} '
           'in your web browser.', file=sys.stderr)
 
 
