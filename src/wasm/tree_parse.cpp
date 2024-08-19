d02276cd78f4dc6d96d0d908331e66d12715807a
Irina Bradu <irina.bradu@cern.ch>
2024-08-19 13:47:01 +0200

    change walltime value to time interval overlap value when prunning the tree

--- src/wasm/tree_parse.cpp
+++ src/wasm/tree_parse.cpp
@@ -51,12 +51,18 @@ TreeNode prune_tree(const TreeNode& node, uint64_t threshold_left, uint64_t thre
     TreeNode pruned_node = node;
 
     pruned_node.children.clear();
+
     for (const auto& child : node.children) {
         //this includes all the functions that have any overlap with the time between the two thresholds
-        if ((child.left_sum + child.value) >= threshold_left && (child.left_sum) <= threshold_right) { 
+        if ((child.left_sum + child.value) > threshold_left && (child.left_sum) < threshold_right) {
             pruned_node.children.push_back(prune_tree(child, threshold_left, threshold_right));
         }
     }
+
+    uint64_t extra_time_left = threshold_left > node.left_sum ? threshold_left - node.left_sum : 0;
+    uint64_t extra_time_right = (node.left_sum + node.value) > threshold_right ? node.left_sum + node.value - threshold_right : 0;
+    pruned_node.value = node.value - extra_time_left - extra_time_right; //substract extra left + right time intervals from walltime if any
+
     return pruned_node;
 }
 
