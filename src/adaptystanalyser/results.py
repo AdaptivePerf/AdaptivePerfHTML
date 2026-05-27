57a38f0e759ddb321eef7acf4c9dfc1e09c674cf
Maksymilian Graczyk <maksymilian.graczyk@cern.ch>
2026-05-27 17:58:59 +0200

    Start implementing proper Python API

--- src/adaptystanalyser/results.py
+++ src/adaptystanalyser/results.py
@@ -5,6 +5,8 @@ import json
 import yaml
 import random
 import html
+from abc import ABC, abstractmethod
+from typing import Union, Self
 from pathlib import Path
 from collections import defaultdict
 
@@ -20,7 +22,8 @@ class Identifier:
         of the supplied result folder.
 
         :param pathlib.Path result: A performance analysis session folder.
-        :raises ValueError: When a provided folder doesn't exist or is incorrect.
+        :raises ValueError: When a provided folder doesn't exist or is
+                            incorrect.
         """
         if not (result / 'dirmeta.json').exists():
             raise ValueError(str(result / 'dirmeta.json') + ' does not exist!')
@@ -35,7 +38,8 @@ class Identifier:
            'minute' not in metadata or \
            'second' not in metadata or \
            'label' not in metadata:
-            raise ValueError('The metadata do not have all the required fields!')
+            raise ValueError('The metadata do not have all the required '
+                             'fields!')
 
         self._year, self._month, self._day, self._hour, self._minute, \
             self._second, self._label = metadata['year'], \
@@ -68,7 +72,7 @@ class Identifier:
         else:
             self._second = str(self._second)
 
-        self._id_str = result.name
+        self._path = result.resolve()
 
     def __str__(self):
         """
@@ -112,7 +116,11 @@ class Identifier:
 
     @property
     def value(self):
-        return self._id_str
+        return str(self) + ': ' + str(self._path)
+
+    @property
+    def path(self):
+        return self._path
 
     def __eq__(self, other):
         return self.value == other.value
@@ -121,21 +129,98 @@ class Identifier:
         return hash(self.value)
 
 
-class PerformanceAnalysisResults:
+class Module(ABC):
+    @abstractmethod
+    def get_name(self):
+        pass
+
+
+class Window(ABC):
+    _ids = set()
+
+    def get_arrgmt(windows: Union[Self, list[Self]]):
+        pass
+
+    @abstractmethod
+    def get_module(self) -> Module:
+        pass
+
+    @abstractmethod
+    def get_type(self) -> str:
+        pass
+
+    @abstractmethod
+    def get_constr_params(self) -> list:
+        pass
+
+    @abstractmethod
+    def get_dependencies(self) -> list[Self]:
+        pass
+
+    @abstractmethod
+    def get_data(self):
+        pass
+
+    def get_id(self):
+        pass
+
+    def set_custom_title(self, title: str):
+        self._custom_title = title
+
+    def get_custom_title(self):
+        if hasattr(self, '_custom_title'):
+            return self._custom_title
+        else:
+            return None
+
+    def is_collapsed(self):
+        if hasattr(self, '_collapsed') and \
+           self._collapsed is not None:
+            return self._collapsed
+        else:
+            return False
+
+    def set_collapsed(self, collapsed: bool):
+        self._collapsed = collapsed
+
+    def to_json(self, identifier: str) -> str:
+        to_return = {
+            'id': identifier,
+            'type': self.get_type(),
+            'constr': self.get_constr_params(),
+            'dependencies': [],
+            'collapsed': self.is_collapsed()
+        }
+
+        module = self.get_module()
+        custom_title = self.get_custom_title()
+        data = self.get_data()
+
+        if module is not None:
+            to_return['module'] = module.get_name()
+
+        if custom_title is not None:
+            to_return['custom_title'] = custom_title
+
+        if data is not None:
+            to_return['data'] = data
+
+        return json.dumps(to_return)
+
+
+class Session:
     """
-    A class describing the results of a specific performance analysis
-    session stored inside a given results directory.
+    A class describing a specific performance analysis session.
     """
 
-    def get_all_folders(path_str: str) -> list:
+    def get_all_sessions(path_str: str) -> list:
         """
-        Get the folders of all performance analysis sessions stored in
-        a given results directory.
+        Get the identifiers of all performance analysis sessions stored in
+        a given directory.
 
-        :param str path_str: The path string to a performance analysis
-                             results directory.
-        :return: The list of folders that can be used
-                 for constructing a PerformanceAnalysisResults object.
+        :param str path_str: The string path to a directory.
+        :return: The list of all session identifiers (in form of Identifier
+                 objects) detected inside the provided directory.
         """
         ids = []
         path = Path(path_str)
@@ -156,22 +241,30 @@ class PerformanceAnalysisResults:
                                                -x.second,
                                                x.label)))
 
-    def __init__(self, performance_analysis_storage: str, folder: str):
+    def __init__(self, identifier: Union[Identifier, Path, str]):
         """
-        Construct a PerformanceAnalysisResults object.
+        Construct a Session object.
 
-        :param str performance_analysis_storage: The path string to a
-                                                 performance analysis
-                                                 results directory.
-        :param str folder: The folder of a performance analysis session
-                           stored inside the results directory.
-                           Call get_all_folders() for the list of all
-                           valid folders.
+        :param identifier: The identifier of a performance analysis
+                           session to be loaded. It can be either
+                           an Identifier object obtained from
+                           get_all_sessions(), a pathlib.Path object
+                           representing the path to a performance
+                           analysis session folder, or a string path
+                           to the same folder.
         """
-        self._path = Path(performance_analysis_storage) / folder
-        self._identifier = Identifier(self._path)
+        if isinstance(identifier, Identifier):
+            self._identifier = identifier
+        elif isinstance(identifier, Path):
+            self._identifier = Identifier(identifier)
+        elif isinstance(identifier, str):
+            self._identifier = Identifier(Path(identifier))
+        else:
+            raise ValueError('identifier must be of type Identifier, '
+                             'pathlib.Path, or str!')
 
-        with (self._path / 'system' / 'system.yml').open(mode='r') as f:
+        with (self._identifier.path / 'system' /
+              'system.yml').open(mode='r') as f:
             self._system = yaml.safe_load(f)
 
         self._used_module_vers = defaultdict(lambda: defaultdict(dict))
@@ -180,8 +273,8 @@ class PerformanceAnalysisResults:
             for node, settings in entity['nodes'].items():
                 for mod in settings['modules']:
                     name = mod['name']
-                    mod_meta_path = self._path / 'system' / entity_name / \
-                        node / name / 'dirmeta.json'
+                    mod_meta_path = self._identifier.path / 'system' / \
+                        entity_name / node / name / 'dirmeta.json'
 
                     if not mod_meta_path.exists():
                         continue
@@ -197,18 +290,19 @@ class PerformanceAnalysisResults:
 
         self._entity_colours = {}
 
-        # TODO: Move entity colour assinging to Adaptyst itself or
+        # TODO: Move entity colour assigning to Adaptyst itself or
         # remove it altogether
 
-        # if (self._path / 'entity_colours.json').exists():
-        #     with (self._path / 'entity_colours.json').open(mode='r') as f:
+        # if (self._identifier.path / 'entity_colours.json').exists():
+        #     with (self._identifier.path /
+        #           'entity_colours.json').open(mode='r') as f:
         #         self._entity_colours = json.load(f)
         # else:
         #     self._entity_colours = {}
 
         self._entity_exit_codes = {}
 
-        for entity_dir in (self._path / 'system').glob('*'):
+        for entity_dir in (self._identifier.path / 'system').glob('*'):
             if not entity_dir.is_dir():
                 continue
 
@@ -224,13 +318,14 @@ class PerformanceAnalysisResults:
     def _set_entity_colour(self, entity, colour):
         self._entity_colours[entity] = colour
 
-        # TODO: Move entity colour assinging to Adaptyst itself or
+        # TODO: Move entity colour assigning to Adaptyst itself or
         # remove it altogether
 
-        # with (self._path / 'entity_colours.json').open(mode='w') as f:
+        # with (self._identifier.path /
+        #       'entity_colours.json').open(mode='w') as f:
         #     f.write(json.dumps(self._entity_colours))
 
-    def get_system_graph(self):
+    def get_system_graph_json(self, json_type: str = 'sigma.js'):
         used_colours = set()
         entities = {}
 
@@ -253,61 +348,70 @@ class PerformanceAnalysisResults:
                               random.randrange(100, 181, 10))
 
                 used_colours.add(colour)
-                entities[entity][1] = f'#{colour[0]:02x}{colour[1]:02x}{colour[2]:02x}'
+                entities[entity][1] = \
+                    f'#{colour[0]:02x}{colour[1]:02x}{colour[2]:02x}'
                 self._set_entity_colour(entity, entities[entity][1])
 
-        return json.dumps({
-            'entities': entities,
-            'system': {
-                'options': {
-                    'allowSelfLoops': False,
-                    'multi': False,
-                    'type': 'directed'
-                },
-                'nodes': [
-                    {
-                        'key': f'{entity}_{k}',
-                        'attributes': {
-                            'x': random.random(),
-                            'y': random.random(),
-                            'label': f'[{entity}] {k}',
-                            'server_id': k,
-                            'size': 40,
-                            'color': entities[html.escape(entity)][1],
-                            'entity': entity,
-                            'backends': [[x['name'],
-                                          self._used_module_vers[entity][k].get(x['name'], [])]
-                                          for x in v['modules']]
+        if json_type == 'sigma.js':
+            return json.dumps({
+                'entities': entities,
+                'system': {
+                    'options': {
+                        'allowSelfLoops': False,
+                        'multi': False,
+                        'type': 'directed'
+                    },
+                    'nodes': [
+                        {
+                            'key': f'{entity}_{k}',
+                            'attributes': {
+                                'x': random.random(),
+                                'y': random.random(),
+                                'label': f'[{entity}] {k}',
+                                'server_id': k,
+                                'size': 40,
+                                'color': entities[html.escape(entity)][1],
+                                'entity': entity,
+                                'backends': [[x['name'],
+                                              self._used_module_vers[
+                                                  entity][k].get(
+                                                      x['name'], [])]
+                                             for x in v['modules']]
+                            }
                         }
-                    }
-                    for entity in self._system['entities'].keys()
-                    for k, v in self._system['entities'][entity]['nodes'].items()
-                ],
-                'edges': [
-                    {
-                        'key': f'{entity}_{k}',
-                        'source': f'{entity}_{v["from"]}',
-                        'target': f'{entity}_{v["to"]}',
-                        'undirected': False,
-                        'attributes': {
-                            'label': k,
-                            'size': 10
+                        for entity in self._system['entities'].keys()
+                        for k, v in self._system['entities'][
+                                entity]['nodes'].items()
+                    ],
+                    'edges': [
+                        {
+                            'key': f'{entity}_{k}',
+                            'source': f'{entity}_{v["from"]}',
+                            'target': f'{entity}_{v["to"]}',
+                            'undirected': False,
+                            'attributes': {
+                                'label': k,
+                                'size': 10
+                            }
                         }
-                    }
-                    for entity in self._system['entities'].keys()
-                    for k, v in self._system['entities'][entity].get('edges', {}).items()
-                ] + [
-                    {
-                        'key': k,
-                        'source': f'{v["from"]["entity"]}_{v["from"]["node"]}',
-                        'target': f'{v["to"]["entity"]}_{v["to"]["node"]}',
-                        'undirected': False,
-                        'attributes': {
-                            'label': k,
-                            'size': 10
+                        for entity in self._system['entities'].keys()
+                        for k, v in self._system['entities'][
+                                entity].get('edges', {}).items()
+                    ] + [
+                        {
+                            'key': k,
+                            'source': f'{v["from"]["entity"]}' +
+                            f'_{v["from"]["node"]}',
+                            'target': f'{v["to"]["entity"]}_{v["to"]["node"]}',
+                            'undirected': False,
+                            'attributes': {
+                                'label': k,
+                                'size': 10
+                            }
                         }
-                    }
-                    for k, v in self._system.get('edges', {}).items()
-                ]
-            }
-        })
+                        for k, v in self._system.get('edges', {}).items()
+                    ]
+                }
+            })
+        else:
+            raise ValueError('json_type must be one of: "sigma.js"')
